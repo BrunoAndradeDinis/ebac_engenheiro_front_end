396b681a2b06c004e0fb44f44c2161331be604b3
Bruni <bruno.a.dinis96@gmail.com>
2024-11-22 02:28:49 -0300

    feat-fix: atualizado o código conforme solicitado pelo retorno do tutor ajustes: - js: condições e interações diretas com os elementos de mensagem de retorno, mensagem de sucesso e mensagem de erro - Html: alteração na estrutura para a mensagem de sucesso e erro - CSS: alteração na estilização das tags de retorno da mensagem de sucesso e erro

--- Pilares do desenvolvimento Front-End/Exercicios/Exercicio módulo 7/src/js/main.js
+++ Pilares do desenvolvimento Front-End/Exercicios/Exercicio módulo 7/src/js/main.js
@@ -1,40 +1,37 @@
 // Declaração das variáveis pegando os elementos
 
-const form = document.getElementById('formulario');
-const campoA = document.getElementById('campoA');
-const campoB = document.getElementById('campoB');
-const retorno = document.getElementById('retorno')
+const form = document.getElementById("formulario");
+const campoA = document.getElementById("campoA");
+const campoB = document.getElementById("campoB");
+const retorno = document.getElementById("retorno");
 
-// validação assim que alterar o Campo B
-campoB.addEventListener('change', ()=>{
-  let valor1 = campoA.value
-  let valor2 = campoB.value
-
-  if(valor1 > valor2 || valor1 === valor2){
-    document.querySelector('.error-message').style.display = 'block'
-    retorno.style.display = "none"
-    return
-  } else {
-    document.querySelector('.error-message').style.display = 'none'
-  }
-})
-
-form.addEventListener('submit', (e)=>{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let valor1 = campoA.value
-  let valor2 = campoB.value
+  let valor1 = parseInt(campoA.value);
+  let valor2 = parseInt(campoB.value);
 
-  // validação para a mensagem  de sucesso, validando os valores, caso o valor do campo a for maior que o do campoB ele retorna a mensagem de erro voltando ao inicio do form.
-  if(valor1 < valor2){
-    retorno.innerHTML = `O Campo B: ${valor2} é maior que o Campo A: ${valor1}`
-    document.querySelector('.error-message').style.display = 'none'
-    retorno.style.display = "block"
+  // validação para a mensagem  de sucesso/erro, validando os valores, caso o valor do campo a for maior que o do campoB ele retorna a mensagem de erro voltando ao inicio do form.
+  if (valor1 < valor2) {
+    retorno.innerHTML = `Sucesso! O Campo B: ${valor2} é maior que o Campo A: ${valor1}`;
+    retorno.style.display = "block";
+
+    if (retorno.classList.contains("error-message")) {
+      retorno.classList.remove("error-message");
+      retorno.classList.add("sucess-message");
+    } else {
+      retorno.classList.add("sucess-message");
+    }
   } else {
-    document.querySelector('.error-message').style.display = 'block'
-    retorno.style.display = 'none'
-    return
+    if (!retorno.classList.contains("error-message")) {
+      retorno.classList.remove("sucess-message");
+      retorno.classList.add("error-message");
+      retorno.innerHTML = "Erro: O Campo B não pode ser menor que o Campo A";
+      campoB.focus()
+    } else {
+      retorno.classList.add("error-message");
+      campoB.focus()
+    }
+    return;
   }
-
-})
-
+});
