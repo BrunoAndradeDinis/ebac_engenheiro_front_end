8ee6b26d7dcba0d3457a84a5b23aa275beb9d55e
Bruni <bruno.a.dinis96@gmail.com>
2024-12-07 20:14:50 -0300

    fixfeat: ajustes em html + css + js. - Html: ajuste na estrutura para compatibilidade do css e melhor visualização da contagem de tarefas do Jquery - Css: ajustes nos botões e hover - Js: ajustes de fadeIn, hide e fadeOut para interação com o html

--- Integração do HTML com JavaScript/Exercícios/Exercício módulo 9/src/js/main.js
+++ Integração do HTML com JavaScript/Exercícios/Exercício módulo 9/src/js/main.js
@@ -1,39 +1,47 @@
 $(document).ready(function () {
   const tabela = $("tbody").get(0); // obtendo o DOM da tbody, para o uso od
   const formulario = $("form");
-  const botaoEditar = '<button id="editar">✍ </button>';
+  const botaoEditar = '<button id="editar">Editar </button>';
 
-  // evento de envio do formulário
+  // evento de envio do formulário e adicionar o item
   formulario.on("submit", (e) => {
     e.preventDefault();
     const inputTarefa = $("#tarefa");
-    const listaTarefas = $('#lista-tarefas')
-    const novaLinha = $('<li>');
-    const celulaTarefa = $('<div>');
-    const celulaDeInteracao = $('<div>');
+    const listaTarefas = $("#lista-tarefas");
+    const novaLinha = $("<li>");
+    const celulaTarefa = $("<div>");
+    const celulaDeInteracao = $("<div>");
+    novaLinha.hide();
 
-    listaTarefas.append(novaLinha)
+    if (inputTarefa.val().length < 3) {
+      alert("Digite pelo menos 3 caracteres");
+      return;
+    }
+
+    listaTarefas.append(novaLinha);
     $(celulaTarefa).append(`<span>${inputTarefa.val()}</span>`);
     $(celulaDeInteracao).append(botaoEditar);
 
-    novaLinha.append(celulaTarefa)
-    novaLinha.append(celulaDeInteracao)
-    inputTarefa.val('')
-    inputTarefa.focus()
-
+    novaLinha.append(celulaTarefa);
+    novaLinha.append(celulaDeInteracao);
+    inputTarefa.val("");
+    inputTarefa.focus();
+    novaLinha.fadeIn("300");
     atualizarDados();
   });
 
   // evento de "riscar" a tarefa
   $("ul").on("click", "div span", function () {
     let li = $(this).closest("li");
-
+    li.hide();
     $(this).toggleClass("line-through");
-
+    li.find("button").hide();
     $(this).hasClass("line-through")
-      ? li.find("button").attr("id", "excluir").text("🗑️")
-      : li.find("button").attr("id", "editar").text("✍");
+      ? li.find("button").attr("id", "excluir").text("Excluir").fadeIn()
+      : li.find("button").attr("id", "editar").text("Editar").fadeIn();
 
+    li.find("button").fadeIn();
+    li.fadeIn("300");
     atualizarDados();
   });
 
@@ -43,14 +51,20 @@ $(document).ready(function () {
     let div = button.closest("div");
     let span = div.siblings("div").find("span"); // .siblings para pegar a td que tem o span
     let input = $("<input>").val(span.text()); // criação de um input com o texto do span
-
+    input.hide();
+    button.hide();
     button.attr("id") === "editar"
-      ? button.text("✅").attr("id", "salvar")
+      ? button.text("Salvar").attr("id", "salvar")
       : button.text("🟥");
 
     button.attr("id") === "salvar"
       ? $(span).replaceWith(input)
       : button.text("🟥");
+
+    input.focus();
+    input.fadeIn("300");
+    button.fadeIn("300");
+    atualizarDados();
   });
 
   //evento de salvar a edição de tarefa
@@ -59,11 +73,17 @@ $(document).ready(function () {
     let td = button.closest("div");
     let input = td.siblings("div").find("input");
     let texto = input.val();
-    let span = $("<span>").text(texto);
 
+    if (texto.length < 3) {
+      alert("Digite pelo menos 3 caracteres");
+      return;
+    }
+
+    let span = $("<span>").text(texto);
     input.replaceWith(span);
 
-    button.attr("id", "editar").text("✍");
+    button.attr("id", "editar").text("Editar");
+    atualizarDados();
   });
 
   // evento de excluir a tarefa
@@ -73,21 +93,23 @@ $(document).ready(function () {
     let li = button.closest("li");
     let span = div.siblings("div").find("span");
 
-    li.remove();
-    atualizarDados();
+    li.fadeOut("300", function () {
+      li.remove();
+      atualizarDados();
+    });
   });
-  atualizarDados();
 
   // atualização de contagem
   function atualizarDados() {
     let tarefasTotal = $("span").length;
-    let tarefasFeitas = $('span').filter(function() {
-      return $(this).css('text-decoration-line') === 'line-through';
+    let tarefasFeitas = $("span").filter(function () {
+      return $(this).css("text-decoration-line") === "line-through";
     }).length;
-    let tarefasAfazer = tarefasTotal - tarefasFeitas
+    let tarefasAfazer = tarefasTotal - tarefasFeitas;
 
-    $('#tarefas-total').text(tarefasTotal)
-    $('#tarefas-a-fazer').text(tarefasAfazer)
-    $('#tarefas-feitas').text(tarefasFeitas)
+    $("#tarefas-total").text(tarefasTotal);
+    $("#tarefas-a-fazer").text(tarefasAfazer);
+    $("#tarefas-feitas").text(tarefasFeitas);
   }
+  atualizarDados();
 });
