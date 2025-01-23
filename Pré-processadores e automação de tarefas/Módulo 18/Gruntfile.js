a9a4ba9094fbff90ccb476c93355f2423a7c227f
Bruni <bruno.a.dinis96@gmail.com>
2025-01-23 16:27:09 -0300

    Módulo 18 - Aula 2 - Crie Tarefas

--- Pré-processadores e automação de tarefas/Módulo 18/Gruntfile.js
+++ Pré-processadores e automação de tarefas/Módulo 18/Gruntfile.js
@@ -1,5 +1,16 @@
-module.exports = (grunt)=>{
+module.exports = (grunt) => {
   grunt.initConfig({
-    pkg:grunt.file.readJSON('package.json'),
-  })
-}
+    pkg: grunt.file.readJSON("package.json"),
+  });
+
+  grunt.registerTask("OlaGrunt", function () {
+    const done = this.async();
+
+    setTimeout(() => {
+      console.log("Olá Grunt!");
+      done();
+    }, 3000);
+  });
+
+  grunt.registerTask("default", ["OlaGrunt"]);
+};
