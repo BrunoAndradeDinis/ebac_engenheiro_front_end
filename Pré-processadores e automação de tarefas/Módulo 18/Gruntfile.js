ac579f2e308550e5495ce9eb35e0bf8641be0f1b
Bruni <bruno.a.dinis96@gmail.com>
2025-01-24 10:04:03 -0300

    Módulo 18 - Aula 3 - Use Grunt para compilar LESS

--- Pré-processadores e automação de tarefas/Módulo 18/Gruntfile.js
+++ Pré-processadores e automação de tarefas/Módulo 18/Gruntfile.js
@@ -1,6 +1,35 @@
+const { option } = require("grunt");
+
 module.exports = (grunt) => {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    less: {
+      development: {
+        files: {
+          "main.css": "main.less",
+        },
+      },
+      production: {
+        options:{
+          compress: true,
+
+        },
+        files: {
+          'main.min.css': 'main.less'
+        }
+      },
+    },
+    sass:{
+      dist: {
+        options:{
+          style: "compressed"
+        },
+        files:{
+          "main2.css":"main.scss"
+        }
+      }
+    }
+
   });
 
   grunt.registerTask("OlaGrunt", function () {
@@ -11,6 +40,9 @@ module.exports = (grunt) => {
       done();
     }, 3000);
   });
+  
+  grunt.loadNpmTasks("grunt-contrib-less"); // garantindo que o grunt contrib esteja carregando.
+  grunt.loadNpmTasks("grunt-contrib-sass"); // garantindo que o grunt contrib esteja carregando.
 
-  grunt.registerTask("default", ["OlaGrunt"]);
+  grunt.registerTask("default", ["less", "sass"]);
 };
