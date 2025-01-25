b8c8d4fec7065bb4ab21ccbaed0fa7bf75fd5ee3
Bruni <bruno.a.dinis96@gmail.com>
2025-01-25 17:16:10 -0300

    Módulo 18 - Aula 07 - Comprima HTML com Grunt

--- Pré-processadores e automação de tarefas/Módulo 18/Aulas 5/Gruntfile.js
+++ Pré-processadores e automação de tarefas/Módulo 18/Aulas 5/Gruntfile.js
@@ -1,4 +1,5 @@
 const { task } = require("grunt");
+const { flatten } = require("lodash");
 
 module.exports = (grunt) => {
   grunt.initConfig({
@@ -21,14 +22,78 @@ module.exports = (grunt) => {
     watch: {
       less: {
         files: ["src/styles/**/*.less"],
-        tasks: ["less:development"]
+        tasks: ["less:development"],
+      },
+      html:{
+        files: ["src/index.html"],
+        tasks: ["replace:dev"]
+      }
+    },
+    replace: {
+      dev: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.css",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["src/index.html"],
+            dest: "dev/",
+          },
+        ],
+      },
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.min.css",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["prebuild/index.html"],
+            dest: "dist/",
+          },
+        ],
       },
     },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          // 1 - minificação
+          // 2 - substituição
+          "prebuild/index.html": "src/index.html",
+        },
+      },
+    },
+    clean: ["prebuild"],
   });
 
   grunt.loadNpmTasks("grunt-contrib-less"); // garantindo que o grunt contrib esteja carregando.
   grunt.loadNpmTasks("grunt-contrib-watch"); // garantindo que o grunt contrib esteja carregando.
+  grunt.loadNpmTasks("grunt-replace"); // garantindo que o grunt replace esteja carregando.
+  grunt.loadNpmTasks("grunt-contrib-htmlmin"); // garantindo que o grunt htmlmin esteja carregando.
+  grunt.loadNpmTasks("grunt-contrib-clean"); // garantindo que o grunt clean esteja carregando.
 
   grunt.registerTask("default", ["watch"]);
-  grunt.registerTask("build", ["less:production"]);
+  grunt.registerTask("build", [
+    "less:production",
+    "htmlmin:dist",
+    "replace:dist",
+    "clean",
+  ]);
 };
