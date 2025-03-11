df04b6ca19aaa2a753ffaf28868dc27fbc551bf2
Bruni <bruno.a.dinis96@gmail.com>
2025-03-11 13:46:09 -0300

    Módulo 24 - aula 5 - Aplique encapsulamento

--- Outros recursos do JavaScript/Módulo 24 - Orientação a objetos com JavaScript/Aula 6 - Aplique encapsulamento/funcionario.js
+++ Outros recursos do JavaScript/Módulo 24 - Orientação a objetos com JavaScript/Aula 6 - Aplique encapsulamento/funcionario.js
@@ -1,4 +1,4 @@
-// Aula 6
+// Aula 5
 function Pessoa(nome) {
   this.nome = nome;
   this.dizOi = () => {
@@ -11,17 +11,7 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
   this.cargo = cargo;
-  // this.salario = salario;
-  let _salario = salario;
-
-  // Getters and Setters
-  this.getSalario = () => {
-    return   `O salário de ${this.nome} é R$${_salario.toFixed(2)}`;
-  };
-
-  this.setSalario = (valor) => {
-    if (typeof valor === "number") _salario = valor;
-  };
+  this.salario = salario;
 
   this.dizCargo = () => {
     console.log(this.cargo);
@@ -35,10 +25,6 @@ const funcionario1 = new Funcionario("Bruno", "Dev Front-end", 5000);
 pessoa1.dizOi();
 
 console.log(pessoa1);
-console.log(funcionario1.getSalario());
+console.log(funcionario1);
 funcionario1.dizOi();
 funcionario1.dizCargo();
-funcionario1.setSalario("8000");
-console.log(funcionario1.getSalario());
-funcionario1.setSalario(3250);
-console.log(funcionario1.getSalario());
