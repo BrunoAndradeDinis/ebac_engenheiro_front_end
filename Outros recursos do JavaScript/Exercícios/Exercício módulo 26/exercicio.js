15203bcaf00d78d606fbc029df8b99670a924cf2
Bruni <bruno.a.dinis96@gmail.com>
2025-03-25 15:58:56 -0300

    Exercicio módulo 26 finalizado

---
+++ Outros recursos do JavaScript/Exercícios/Exercício módulo 26/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(x, y) {
+    return x * y;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+console.log(multiplicacao(45, 9));
+console.log(saudacao("Bruno"));
