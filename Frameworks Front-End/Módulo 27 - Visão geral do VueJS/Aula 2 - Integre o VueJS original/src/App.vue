ee0c709861e555813733996066e6518e57b68ce1
Bruni <bruno.a.dinis96@gmail.com>
2025-03-28 12:15:53 -0300

    Mòdulo 27 - Aula 3 - Faça a renderização condicional

---
+++ Frameworks Front-End/Módulo 27 - Visão geral do VueJS/Aula 2 - Integre o VueJS original/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+const nome = "Bruno de Andrade";
+const meuObj = {
+  nome: "Bruno",
+  filmeFav: "The Warriors",
+  dizOla(x) {
+    return `${x} diz: Olá!`;
+  },
+};
+const enderecoDaImagem =
+  "https://sm.ign.com/ign_pt/screenshot/default/blob_2mkr.jpg";
+
+</script>
+
+<template>
+  <h1>Olá Mundo!</h1>
+  <h2>{{ nome }}.</h2>
+  <h3>{{ meuObj.dizOla(meuObj.nome) }}</h3>
+  <div>
+    <img v-bind:src="enderecoDaImagem" alt="" width="300px" />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Frameworks Front-End/Módulo 27 - Visão geral do VueJS/Aula 3 - Faça a renderização condicional/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+const nome = "Bruno de Andrade";
+const meuObj = {
+  nome: "Bruno",
+  filmeFav: "The Warriors",
+  dizOla(x) {
+    return `${x} diz: Olá!`;
+  },
+};
+const enderecoDaImagem =
+  "https://sm.ign.com/ign_pt/screenshot/default/blob_2mkr.jpg";
+const enderecoDaImagem2 =
+  "https://i.scdn.co/image/ab67616d0000b273cfeae645958e9248abff0710";
+
+const botaoEstaDesabilitado = true;
+
+const gostaDoSquirtle = true;
+const gostaDoPikachu = false;
+
+const estaAutorizado = false;
+</script>
+
+<template>
+  <h1>Olá Mundo!</h1>
+  <h2>{{ nome }}.</h2>
+  <h3>{{ meuObj.dizOla(meuObj.nome) }}</h3>
+  <div>
+    <img v-if="gostaDoSquirtle" :src="enderecoDaImagem" alt="" />
+    <img v-else-if="gostaDoPikachu" :src="enderecoDaImagem2" alt="" />
+    <h2 v-else>Não curte Pokemóns da primeira geração.</h2>
+  </div>
+  <div></div>
+
+  <h3 v-if="estaAutorizado">Bem vindo!</h3>
+  <h3 v-else>Não possui acesso!</h3>
+
+  <button :disabled="botaoEstaDesabilitado">Enviar mensagem</button>
+</template>
+
+<style scoped>
+img {
+  max-width: 300px;
+  border-radius: 0.5rem;
+}
+</style>
