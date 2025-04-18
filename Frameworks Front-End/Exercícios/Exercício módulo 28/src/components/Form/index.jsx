3beceb1dfa4a98169579116aaefff6f9ef118385
Bruni <bruno.a.dinis96@gmail.com>
2025-04-18 15:47:53 -0300

    Exercicio módulo 29 finalizado!

--- Frameworks Front-End/Exercícios/Exercício módulo 28/src/components/Form/index.jsx
+++ Frameworks Front-End/Exercícios/Exercício módulo 28/src/components/Form/index.jsx
@@ -4,15 +4,13 @@ import {form, form__container, form__container__title, form__container__item, fo
 export default function Form({imcResultado}) {
   const [altura, setAltura] = useState(0)
   const [peso, setPeso] = useState(0)
-  const [imc, setImc]  = useState(0) 
-
+  const [imcCalculado, setImcCalculado] = useState(0)
   const calcular = (e)=>{
     e.preventDefault()
-    let imcCalculado = peso / (altura * 2)
+    setImcCalculado(peso / (altura * 2))
 
     console.log(imcCalculado)
-    setImc(imcCalculado)
-    imcResultado(imc)
+    imcResultado(imcCalculado)
   }
   return (
     <form className={form}>
@@ -20,13 +18,15 @@ export default function Form({imcResultado}) {
         <h1 className={form__container__title}>Calcular IMC</h1>
         <div className={form__container__item}>
           <label htmlFor="altura">Altura:</label>
-          <input type="number" id="altura"  step="0.01" placeholder="1.75" max={2.2} min={.1} onBlur={({target}) => parseFloat(setAltura(target.value))}/>
+          <input type="number" id="altura"  step="0.01" placeholder="1.75" max={2.2} min={.1} onChange={({target}) => setAltura(parseFloat(target.value))}/>
         </div>
         <div className={form__container__item}>
           <label htmlFor="peso">Peso:</label>
-          <input type="number" id="peso"  step="0.01" placeholder="74.50" max={200.2} min={.1} onBlur={({target}) => parseFloat(setPeso(target.value))}/>
+          <input type="number" id="peso"  step="0.01" placeholder="74.50" max={200.2} min={.1} onChange={({target}) => setPeso(parseFloat(target.value))}/>
         </div>
         <button className={form__container__button} onClick={calcular}>Calcular</button>
+
+        <p>{imcCalculado === 0 ? "" : `Resuldado IMC: ${imcCalculado.toFixed(2)}`}</p>
       </div>
     </form>
   );
--- Frameworks Front-End/Exercícios/Exercício módulo 28/src/components/Main/index.jsx
+++ Frameworks Front-End/Exercícios/Exercício módulo 28/src/components/Main/index.jsx
@@ -1,43 +1,99 @@
+import { useState } from "react";
 import {
   main,
-  main__title,
   main__container,
   main__container__item,
+  pesoAbaixo,
+  pesoNormal,
+  sobrepeso,
+  obesidadeI,
+  obesidadeII,
+  obesidadeIII,
 } from "./Main.module.css";
+import { useEffect } from "react";
 
-export default function Main() {
+export default function Main({ imcResultado }) {
+  const [qualImc, setQualImc] = useState("");
 
-  
+  const validaImc = (imc) => {
+    switch (true) {
+      case imc > 0 && imc < 1:
+        return "IMC Não calculado"
+      case imc > 1 && imc < 18.5:
+        return "pesoAbaixo";
+      case imc >= 18.5 && imc < 24.9:
+        return "pesoNormal";
+      case imc >= 25 && imc < 29.9:
+        return "sobrepeso";
+      case imc >= 30 && imc < 34.9:
+        return "obesidadeI";
+      case imc >= 35 && imc < 39.9:
+        return "obesidadeII";
+      case imc >= 40:
+        return "obesidadeIII";
+      default:
+        return "IMC Não calculado";
+    }
+  };
+
+  useEffect(() => {
+    setQualImc(validaImc(imcResultado));
+    console.log(qualImc)
+  }, [imcResultado, qualImc]);
   return (
     <main className={main}>
-      <h2 className={main__title}>Seu IMC</h2>
+      <h2>Seu IMC</h2>
       <div className={main__container}>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "pesoAbaixo" ? pesoAbaixo : ""
+          }`}
+        >
           <p>
             <strong>Abaixo do peso:</strong> IMC abaixo de 18,5.
           </p>
         </div>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "pesoNormal" ? pesoNormal: ""
+          }`}
+        >
           <p>
             <strong>Peso normal:</strong> IMC entre 18,5 e 24,9.
           </p>
         </div>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "sobrepeso" ? sobrepeso : ""
+          }`}
+        >
           <p>
             <strong>Sobrepeso:</strong> IMC entre 25 e 29,9.
           </p>
         </div>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "obesidadeI" ? obesidadeI : ""
+          }`}
+        >
           <p>
             <strong>Obesidade I:</strong> IMC entre 30 e 34,9.
           </p>
         </div>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "obesidadeII" ? obesidadeII : ""
+          }`}
+        >
           <p>
             <strong>Obesidade II:</strong> IMC entre 35 e 39,9.
           </p>
         </div>
-        <div className={main__container__item}>
+        <div
+          className={`${main__container__item} ${
+            qualImc === "obesidadeIII" ? obesidadeIII : ""
+          }`}
+        >
           <p>
             <strong>Obesidade III:</strong> IMC acima de 40.
           </p>
