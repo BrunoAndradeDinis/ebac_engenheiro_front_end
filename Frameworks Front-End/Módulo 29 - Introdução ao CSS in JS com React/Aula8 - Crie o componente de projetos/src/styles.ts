4f098d5edb084bdc10a512c813f7ecad4a073577
Bruni <bruno.a.dinis96@gmail.com>
2025-04-29 12:06:37 -0300

    Módulo 29 - Aula 8 - Crie o componente dos projetos

---
+++ Frameworks Front-End/Módulo 29 - Introdução ao CSS in JS com React/Aula8 - Crie o componente de projetos/src/styles.ts
@@ -0,0 +1,41 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+const EstiloGlobal = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+    transition: all .3s ease-in-out;
+
+    body{
+      padding-top: 80px;
+      background-color: antiquewhite;
+
+      @media (max-width: 768px) {
+        padding-top: 16px;
+      }
+    }
+  }
+`
+
+export default EstiloGlobal
+
+export const Container = styled.div`
+  max-width: 1074px;
+  width: 100%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 128px auto;
+  column-gap: 56px;
+
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`
