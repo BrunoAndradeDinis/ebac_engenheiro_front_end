fe451e27cb2f7adbbe74088a9a3bed2a8d46bd21
Bruno de Andrade Dinis <bruno.a.dinis96@gmail.com>
2025-05-05 23:46:45 -0300

    Exercício módulo 29 - Finalizado

---
+++ Frameworks Front-End/Exercícios/Exercício módulo 29/src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { FormContainer, Formulario, HeroTitle } from './styles'
+
+const Hero = () => (
+  <Formulario>
+    <FormContainer>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </FormContainer>
+  </Formulario>
+)
+
+export default Hero
