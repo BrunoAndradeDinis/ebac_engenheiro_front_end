65b6688c0169204566b2c3fedc248bcbd0c579d3
Bruni <bruno.a.dinis96@gmail.com>
2025-07-06 10:31:31 -0300

    Módulo 31 - Aula 3 - Construa o componente de filtro

---
+++ Frameworks Front-End/Módulo 31 - Projeto 5 - Parte 1/Aula 3 - Construa o componente de filtro/src/components/FiltroCard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { Props } from '.'
+
+export const Card = styled.div<Props>`
+  padding: 0.5rem;
+  border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
+  background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
+  color: ${(props) => (props.ativo ? '#1e90ff' : '#5e5e5e')};
+  border-radius: 0.5rem;
+`
+export const Contador = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  display: block;
+`
+export const Label = styled.span`
+  font-size: 14px;
+`
---
+++ Frameworks Front-End/Módulo 31 - Projeto 5 - Parte 1/Aula 3 - Construa o componente de filtro/src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 1rem;
+  background-color: #eee;
+  height: 100vh;
+`
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0.5rem;
+  margin-top: 1rem;
+`
+
+export const Campo = styled.input`
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  background-color: #fff;
+  font-weight: bold;
+  color: #666666;
+  border-color: #666666;
+  width: 100%;
+`
