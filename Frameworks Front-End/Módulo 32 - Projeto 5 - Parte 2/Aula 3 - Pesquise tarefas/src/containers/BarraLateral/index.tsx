0fb149d8fe7bfd2591ac0e84f1d6f79584c7aedf
Bruni <bruno.a.dinis96@gmail.com>
2025-07-17 23:58:55 -0300

    Módulo 32 - Aula 3 - Pesquise Tarefas

---
+++ Frameworks Front-End/Módulo 32 - Projeto 5 - Parte 2/Aula 3 - Pesquise tarefas/src/containers/BarraLateral/index.tsx
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux'
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
+
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Prourar"
+          value={termo}
+          onChange={(e) => dispatch(alteraTermo(e.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard contador={3} legenda="pendentes" />
+          <FiltroCard contador={4} legenda="concluídas" />
+          <FiltroCard contador={2} legenda="urgentes" />
+          <FiltroCard contador={2} legenda="importantes" />
+          <FiltroCard contador={3} legenda="normal" />
+          <FiltroCard ativo contador={7} legenda="todas" />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
+
+export default BarraLateral
