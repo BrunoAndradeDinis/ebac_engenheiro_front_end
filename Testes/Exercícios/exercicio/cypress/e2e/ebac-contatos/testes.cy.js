9f3ed4cd02a12738772ab39aa8d6001612289900
Bruni <bruno.a.dinis96@gmail.com>
2026-01-28 19:14:39 -0300

    --amend

--- Testes/Exercícios/exercicio/cypress/e2e/ebac-contatos/testes.cy.js
+++ Testes/Exercícios/exercicio/cypress/e2e/ebac-contatos/testes.cy.js
@@ -15,11 +15,19 @@ describe("Testes cypres para o site de lista de contatos", () => {
     cy.get('[type="email"]').type("leyserestrada@mailer.com");
     cy.get('[type="tel"]').type("47 91000 1234");
     cy.get(".adicionar").click();
+
+    cy.contains("Leyser Erick Pinto Estrada").should("be.visible");
+    cy.contains("leyserestrada@mailer.com").should("be.visible");
+    cy.contains("47 91000 1234").should("be.visible");
   });
 
   it("deve ativar modo de edição", () => {
     cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
+
     cy.get('[type="text"]').should("have.length.greaterThan", 0);
+    cy.get('[type="email"]').should("have.length.greaterThan", 0);
+    cy.get('[type="tel"]').should("have.length.greaterThan", 0);
+    cy.get(".alterar").should("be.visible");
   });
 
   it("deve alterar o primeiro contato selecionado", () => {
@@ -30,14 +38,23 @@ describe("Testes cypres para o site de lista de contatos", () => {
 
     cy.get('[type="email"]').clear();
     cy.get('[type="email"]').type("jasmielisestrada@mailer.com");
-    
+
     cy.get('[type="tel"]').clear();
     cy.get('[type="tel"]').type("47 91000 1234");
-    
+
     cy.get(".alterar").click();
+
+    cy.contains("Jasmielis Leysimar Pinto Estrada").should("be.visible");
+    cy.contains("jasmielisestrada@mailer.com").should("be.visible");
   });
 
   it("Deve apagar o ultimo contato", () => {
-    cy.get(".delete").last().click();
+
+    cy.get(".sc-gueYoa").then(($contatos) => {
+      const contatosAntesDelete = $contatos.length;
+      cy.get(".delete").last().click();
+
+      cy.get(".sc-gueYoa").should("have.length", contatosAntesDelete - 1);
+    });
   });
 });
