2cc5416d56db52f3cbce0ba7a2fd82d949766de1
Bruno de Andrade Dinis <bruno.a.dinis96@gmail.com>
2026-04-06 19:55:13 -0300

    Refactor code structure for improved readability and maintainability

---
+++ Entrevista, carreira e portfólio/Módulo 36 - Projeto 6 - Parte 2/Aula 1 - Crie a página de detalhes/aula1.md
@@ -0,0 +1,5 @@
+Neste momento, você criará a página de detalhes. Dê play na videoaula para:
+
+- configurar e compreender o sistema de rotas no React;
+- criar uma página de detalhes do produto interativa;
+- utilizar a desestruturação e manipulação de parâmetros de rota.
--- Entrevista, carreira e portfólio/eplay/src/Pages/routes.tsx
+++ Entrevista, carreira e portfólio/eplay/src/Pages/routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Categories from './Categories'
+import Product from './Product'
 
 // const rotas = createBrowserRouter([
 //   {
@@ -17,6 +18,7 @@ const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product" element={<Product />} />
   </Routes>
 )
 
